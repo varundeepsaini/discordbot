41f63d50e66bbe060b3dde217028347d359f8b33
Varun Deep Saini <varun.23bcs10048@ms.sst.scaler.com>
2023-09-23 16:35:48 +0530

    Added the Upcoming Contests Slash Command Solves #1

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from discord import ApplicationContext, Bot
 from utils.env import DISCORD_BOT_TOKEN
+import os
+bot = Bot()
 
-bot  = Bot()
 
 @bot.command(description="Pinggg")
 async def ping(ctx: ApplicationContext):
     await ctx.respond(f"Pong! Latency is {bot.latency}")
 
+
 @bot.event
 async def on_ready():
-    print("Saini Certified is online.")
+    print(f"Saini Certified is online.")
+
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+        print(f'Loaded {filename[:-3]} cog')
+
 
 bot.run(DISCORD_BOT_TOKEN)
+
+
