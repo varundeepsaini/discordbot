41aa8a165dfc8b7882300e982741d98e42bf4fa1
Arth <probablyarth@gmail.com>
2023-09-23 17:04:37 +0530

    solves #11

--- main.py
+++ main.py
@@ -13,13 +13,15 @@ async def ping(ctx: ApplicationContext):
 async def on_ready():
     print(f"Saini Certified is online.")
 
-
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         bot.load_extension(f'cogs.{filename[:-3]}')
         print(f'Loaded {filename[:-3]} cog')
+    else:
+        for subFileName in os.listdir(f'./cogs/{filename}'):
+            if subFileName.endswith('.py'):
+                bot.load_extension(f'cogs.{filename}.{subFileName[:-3]}')
+                print(f'Loaded {filename}.{subFileName[:-3]} cog')
 
 
 bot.run(DISCORD_BOT_TOKEN)
-
-
