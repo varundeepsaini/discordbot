c4f45d428d0b4ae0225e46250b07d3661de332de
Arth <probablyarth@gmail.com>
2023-09-24 11:46:42 +0530

    Fixed codeforces KeyError

--- cogs/cp_profiles/codeforces.py
+++ cogs/cp_profiles/codeforces.py
@@ -1,24 +1,33 @@
-from discord import *
+from discord import Embed
 from discord.ext import commands
 import requests
 
 
-def codeforces_embed(formatted_response):
-    handle = formatted_response['result'][0]['handle']
-    name = formatted_response['result'][0]['firstName'] + " " + formatted_response['result'][0]['lastName']
-    rank = formatted_response['result'][0]['rank']
-    max_rank = formatted_response['result'][0]['maxRank']
-    rating = formatted_response['result'][0]['rating']
-    max_rating = formatted_response['result'][0]['maxRating']
+def codeforces_embed(formatted_response: dict):
+    embed = Embed(title="Codeforces Profile", description="Here is your Codeforces Profile.", color=0x00ff00)
+    keys = formatted_response.keys()
+    handle = formatted_response['handle']
+    if ("firstName" in keys):
+        name = formatted_response['firstName'] + " " + formatted_response['lastName']
+        embed.add_field(name="Name", value=name, inline=False)
+    if ("rank" in keys):    
+        rank = formatted_response['rank']
+        embed.add_field(name="Rank", value=rank.capitalize(), inline=False)
+
+    if ("maxRank" in keys):
+        max_rank = formatted_response['maxRank']
+        embed.add_field(name="Max Rank", value=max_rank.capitalize(), inline=False)
+
+    if ("rating" in keys):
+        rating = formatted_response['rating']
+        max_rating = formatted_response['maxRating']
+        embed.add_field(name="Max Rating", value=max_rating, inline=False)
+    else:
+        rating = "*unrated*"
     profile_url = f"https://codeforces.com/profile/{handle}"
 
-    embed = Embed(title="Codeforces Profile", description="Here is your Codeforces Profile.", color=0x00ff00)
-    embed.add_field(name="Name", value=name, inline=False)
-    embed.add_field(name="Handle", value=f"[{handle}]({profile_url})", inline=False)  # hyperlink the handle
     embed.add_field(name="Rating", value=rating, inline=False)
-    embed.add_field(name="Rank", value=rank.capitalize(), inline=False)
-    embed.add_field(name="Max Rating", value=max_rating, inline=False)
-    embed.add_field(name="Max Rank", value=max_rank.capitalize(), inline=False)
+    embed.add_field(name="Handle", value=f"[{handle}]({profile_url})", inline=False)  # hyperlink the handle
 
     return embed
 
@@ -35,18 +44,17 @@ class codeforces(commands.Cog):
         formatted_response = requests.get(url)
         if formatted_response.status_code != 200:
             print(f"Failed to get data from API. Status code: {formatted_response.status_code}")
-            return []
+            return
         try:
             data = formatted_response.json()
         except ValueError:
             print("Failed to decode JSON data from API response.")
-            return []
-        print(data)
+            return
         if data['status'] == "FAILED":
             await ctx.send("Invalid username")
             return
         else:
-            embed = codeforces_embed(data)
+            embed = codeforces_embed(data["result"][0])
 
         await ctx.send(embed=embed)
 
